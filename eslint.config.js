50814725d6461de9203a1ba9e0e11e00e36ff3b4
ryansparkc <bluegx08@gmail.com>
2026-02-12 12:51:13 +0800

    chore: 調整 ESLint React 規則與未使用變數判定

--- eslint.config.js
+++ eslint.config.js
@@ -1,5 +1,6 @@
 import js from '@eslint/js';
 import globals from 'globals';
+import react from 'eslint-plugin-react';
 import reactHooks from 'eslint-plugin-react-hooks';
 import reactRefresh from 'eslint-plugin-react-refresh';
 import { defineConfig, globalIgnores } from 'eslint/config';
@@ -10,6 +11,8 @@ export default defineConfig([
     files: ['**/*.{js,jsx}'],
     extends: [
       js.configs.recommended,
+      react.configs.flat.recommended,
+      react.configs.flat['jsx-runtime'],
       reactHooks.configs.flat.recommended,
       reactRefresh.configs.vite,
     ],
@@ -22,8 +25,20 @@ export default defineConfig([
         sourceType: 'module',
       },
     },
+    settings: {
+      react: {
+        version: 'detect',
+      },
+    },
     rules: {
-      'no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z_]' }],
+      'no-unused-vars': [
+        'error',
+        {
+          varsIgnorePattern: '^[_A-Z]',
+          argsIgnorePattern: '^[_A-Z]',
+        },
+      ],
+      'react/prop-types': 'off',
       // Data fetching in useEffect is a valid use case for setState
       'react-hooks/set-state-in-effect': 'off',
     },
